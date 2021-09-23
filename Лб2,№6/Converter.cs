8b139eac2b0cab3bab6dee107af0064cbd3e6a19
Денис Зайдун <denzaydunden@gmail.com>
2021-09-23 19:30:13 +0300

    first

---
+++ Лб2,№6/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Лб2__6
+{
+    class Converter
+    {
+        private double _usdToUah, _eurToUah, _rubToUah;
+        private double _usd, _eur, _rub, _uah;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            _usd = usd;
+            _usdToUah = Math.Round(_usd * 26.69, 2);
+
+            _eur = eur;
+            _eurToUah = Math.Round(_eur * 31.29, 2);
+
+            _rub = rub;
+            _rubToUah = Math.Round(_rub * 0.37, 2);
+        }
+
+        public Converter(double uah)
+        {
+            _uah = uah;
+            _usd = Math.Round(_uah / 26.69, 2);
+            _eur = Math.Round(_uah / 31.29, 2);
+            _rub = Math.Round(_uah / 0.37, 2);
+        }
+
+        public void InfoCurrToUah()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{_usd}$ = {_usdToUah}₴");
+            Console.WriteLine($"{_eur}€ = {_eurToUah}₴");
+            Console.WriteLine($"{_rub}₱ = {_rubToUah}₴");
+            Console.ResetColor();
+        }
+
+        public void InfoUahToCurr()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{_uah}₴ = {_usd}$");
+            Console.WriteLine($"{_uah}₴ = {_eur}€");
+            Console.WriteLine($"{_uah}₴ = {_rub}₱");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Лб2,№6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Лб2__6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            int ans = 0;
+            int secondAns = 0;
+
+            do
+            {
+                Console.WriteLine("Оберіть:");
+                Console.WriteLine("1. Конвертувати з гривні у валюту(USD, EUR, RUB).");
+                Console.WriteLine("2. Конвертувати з валюти(USD, EUR, RUB) в гривню.");
+                Console.Write("Ваш вибір: ");
+                ans = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (ans == 1 || ans == 2)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введіть 1 або 2");
+                    Console.ResetColor();
+                }
+            } while (ans != 1 || ans != 2);
+
+
+            if (ans == 1)
+            {
+                Console.WriteLine("Ви обрали конвертувати з гривні у валюту(USD, EUR, RUB)");
+                Console.Write("Введіть суму в гривнях:");
+                double uah = double.Parse(Console.ReadLine());
+
+                Converter converter = new Converter(uah);
+
+                Console.WriteLine();
+                converter.InfoUahToCurr();
+            }
+            else if (ans == 2)
+            {
+                do
+                {
+                    Console.WriteLine("Ви обрали конвертувати з валюти в гривню");
+                    Console.WriteLine("Оберіть:");
+                    Console.WriteLine("1. Задати значення валют(USD, EUR, RUB) одночасно");
+                    Console.WriteLine("2. Задати значення валют(USD, EUR, RUB) окремо");
+                    Console.Write("Ваш вибір: ");
+                    secondAns = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (secondAns == 1 || secondAns == 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Введіть 1 або 2");
+                        Console.ResetColor();
+                    }
+                } while (secondAns != 1 || secondAns != 2);
+
+
+
+                if (secondAns == 1)
+                {
+                    Console.Write("Введіть суму: ");
+                    double val = double.Parse(Console.ReadLine());
+
+                    Converter converter = new Converter(val, val, val);
+
+                    Console.WriteLine();
+                    converter.InfoCurrToUah();
+                }
+                else if (secondAns == 2)
+                {
+                    Console.Write("Введіть суму в доларах: ");
+                    double usd = double.Parse(Console.ReadLine());
+
+                    Console.Write("Введіть суму в євро: ");
+                    double eur = double.Parse(Console.ReadLine());
+
+                    Console.Write("Введіть суму в рублях: ");
+                    double rub = double.Parse(Console.ReadLine());
+
+                    Converter converter = new Converter(usd, eur, rub);
+
+                    Console.WriteLine();
+                    converter.InfoCurrToUah();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
